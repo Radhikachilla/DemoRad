48260d62499877d8e369242a377cbaafc288b9de
Radhikachilla <radhikavchilla@gmail.com>
2025-06-10 10:41:03 +0530

    Update App.java

--- src/main/java/Trial/MavenTrial/App.java
+++ src/main/java/Trial/MavenTrial/App.java
@@ -1,10 +1,13 @@
 package Trial.MavenTrial;
-
+import java.util.scanner;
 /**
  * Hello world!
  */
 public class App {
+    Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Enter your name:");
+         char name=sc.nextInt();
     }
 }
